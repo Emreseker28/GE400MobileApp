89a8e0869586c1d5b954ac71cf532e446ccc6849
berkcanylmz <105168071+berkcanylmz@users.noreply.github.com>
2022-05-10 23:25:38 +0300

    done

--- ge400mobileapp/lib/HomePage.dart
+++ ge400mobileapp/lib/HomePage.dart
@@ -3,17 +3,23 @@ import 'package:flutter/widgets.dart';
 import 'package:ge400mobileapp/globals.dart';
 
 class HomePage extends ChangeNotifier{
-  get isVisible => isVisible;
+  
   bool _isVisible = false;
-  set isVisible(value){
+  get getIsVisible{
+    return _isVisible;
+  }
+  void isVisible(value){
     _isVisible = value;
     notifyListeners();
   }
 
   get isValid => _isValid;
   bool _isValid = false;
+  get getValid{
+    return _isValid;
+  }
   void isValidEmail(String input){
-    if(input = Global.validEmail.first){
+    if(input == Global.validEmail.first){
       _isValid  = true;
     } else {
       _isValid = false;
